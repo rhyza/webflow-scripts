668fe3bf508f8cda6eae01f914cab62fb95bf968
rhyza <rhyza.velasco@gmail.com>
2022-02-03 03:05:34 -0800

    Create basic shopify product card

---
+++ shopify_product_card.js
@@ -0,0 +1,65 @@
+/**
+* Script that creates an embeddable Shopify Product showing only a photo, name,
+* and price.
+* Use with the corresponding Webflow embed code:
+<div id="product-component-1643788609937"></div>
+<script
+  id="product-component"
+  product-id="{{Shopify Product ID}}"
+  type="text/javascript"
+  src="https://cdn.jsdelivr.net/gh/rhyza/webflow-scripts@latest/shopify_product_card.min.js">
+</script>
+*/
+
+(function () {
+  var scriptURL =
+    "https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js";
+  if (window.ShopifyBuy) {
+    if (window.ShopifyBuy.UI) {
+      ShopifyBuyInit();
+    } else {
+      loadScript();
+    }
+  } else {
+    loadScript();
+  }
+  function loadScript() {
+    var script = document.createElement("script");
+    script.async = true;
+    script.src = scriptURL;
+    (
+      document.getElementsByTagName("head")[0] ||
+      document.getElementsByTagName("body")[0]
+    ).appendChild(script);
+    script.onload = ShopifyBuyInit;
+  }
+  function ShopifyBuyInit() {
+    var client = ShopifyBuy.buildClient(clientData);
+    var scriptId = document.getElementById("product-component");
+    var productId = scriptId.getAttribute("product-id");
+    ShopifyBuy.UI.onReady(client).then(function (ui) {
+      ui.createComponent("product", {
+        id: productId,
+        node: document.getElementById("product-component-1643788609937"),
+        moneyFormat: "%24%7B%7Bamount%7D%7D",
+        options: {
+          product: {
+            styles: {
+              product: {
+                "@media (min-width: 601px)": {
+                  "max-width": "calc(25% - 20px)",
+                  "margin-left": "20px",
+                  "margin-bottom": "50px",
+                },
+              },
+            },
+            contents: {
+              button: false,
+              options: false,
+            },
+          },
+        },
+      });
+    });
+  }
+})();
